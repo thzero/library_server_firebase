190786aaca31bbe920f212404cce86a939cb4c40
thZero <corvette@thzero.com>
2026-09-14 22:04:19 -0500

    licecnse updates

--- openSource.js
+++ openSource.js
@@ -16,17 +16,24 @@ export default () => {
         },
         {
             category: 'server',
-            name: '@thzero/library_service',
-            url: 'https://github.com/thzero/library_service',
+            name: '@thzero/library_server',
+            url: 'https://github.com/thzero/library_server',
             licenseName: 'MIT',
-            licenseUrl: 'https://github.com/thzero/library_service/blob/master/license.md'
+            licenseUrl: 'https://github.com/thzero/library_server/blob/master/license.md'
         },
-		{
-			category: 'server',
-			name: '@thzero/library_server_firebase',
-			url: 'https://github.com/thzero/library_server_firebase',
-			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/thzero/library_server_firebase/blob/master/license.md'
-		}
+        {
+            category: 'server',
+            name: '@thzero/library_server_firebase',
+            url: 'https://github.com/thzero/library_server_firebase',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_server_firebase/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: 'firebase-admin',
+            url: 'https://github.com/firebase/firebase-admin-node',
+            licenseName: 'Apache-2.0',
+            licenseUrl: 'https://github.com/firebase/firebase-admin-node/blob/master/LICENSE'
+        }
     ];
 }
